b6338d373e90ef812f2469c7e7cb12f868eba2fe
sunminkim923 <tjsals0406@gmail.com>
2021-11-14 16:52:40 +0900

    fix getServerSideProps

--- pages/board/detail/[Id]/index.tsx
+++ pages/board/detail/[Id]/index.tsx
@@ -1,5 +1,16 @@
 import BoardDetail from "../../../../src/components/units/board/detail/boardDetail.container";
 import Head from "next/head";
+import request, { gql } from "graphql-request";
+
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
 
 export default function DetailPage(props: any) {
   return (
@@ -8,20 +19,23 @@ export default function DetailPage(props: any) {
         <meta property="og:title" content="MOCAR" />
         <meta
           property="og:image"
-          content="https://mocar.shop/images/boardIMG.jpg"
+          content={`https://storage.googleapis.com/${props?.fetchBoard.images[0]}`}
         />
-
-        <meta property="og:url" content="https://mocar.shop" />
-        <meta
-          property="og:description"
-          content="자동차를 사랑하는 사람들의 커뮤니티 MOCAR 입니다."
-        />
+        <meta property="og:description" content={props?.fetchBoard.contents} />
       </Head>
       <BoardDetail />
     </>
   );
 }
 
-export const getServerSideProps = () => {
-  return;
-};
+// export const getServerSideProps = async (context: any) => {
+//   const result = await request(
+//     "https://backend02.codebootcamp.co.kr/graphql05",
+//     FETCH_BOARD,
+//     {
+//       boardId: context.query.boardId,
+//     }
+//   );
+
+//   return { props: { fetchBoard: result.fetchBoard } };
+// };
